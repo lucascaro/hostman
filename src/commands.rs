4f44231a41ebbe44d9b7180274aff486bd64990b
Lucas Caro <lucascaro@gmail.com>
2019-03-16 20:00:42 -0700

    fmt + clippy

--- src/commands.rs
+++ src/commands.rs
@@ -1,98 +1,99 @@
 use crate::hostsfile::MatchType;
 
 pub fn show(summary: bool) {
-  let hosts_file = crate::hostsfile::ManagedHostsFile::must_load();
-  match summary {
-    false => println!("{}", hosts_file),
-    true => println!("{}", hosts_file.without_comments().join("\n")),
-  }
+    let hosts_file = crate::hostsfile::ManagedHostsFile::must_load();
+    if summary {
+        println!("{}", hosts_file);
+    } else {
+        println!("{}", hosts_file.without_comments().join("\n"));
+    }
 }
 
 pub fn check(host: &str, exact: bool) {
-  let hosts_file = crate::hostsfile::ManagedHostsFile::must_load();
-  let found = hosts_file.get_matches(host, MatchType::from_bool(exact));
-  println!("{}", found.join("\n"));
+    let hosts_file = crate::hostsfile::ManagedHostsFile::must_load();
+    let found = hosts_file.get_matches(host, MatchType::from_bool(exact));
+    println!("{}", found.join("\n"));
 }
 
 pub fn add(ip: &str, names: &str, comment: &str) {
-  let all_names = names.split(',').collect::<Vec<&str>>();
-  let mut hosts_file = crate::hostsfile::ManagedHostsFile::must_load();
-  for name in &all_names {
-    let matches = hosts_file.get_matches(name, MatchType::Exact);
-    if matches.len() > 0 {
-      println!(
-        "The requested host is already present: \n{}",
-        matches.join("\n")
-      );
-      return;
+    let all_names = names.split(',').collect::<Vec<&str>>();
+    let mut hosts_file = crate::hostsfile::ManagedHostsFile::must_load();
+    for name in &all_names {
+        let matches = hosts_file.get_matches(name, MatchType::Exact);
+        if !matches.is_empty() {
+            println!(
+                "The requested host is already present: \n{}",
+                matches.join("\n")
+            );
+            return;
+        }
     }
-  }
-  let names = all_names.join(" ");
-  println!("Adding {} {} to /etc/hosts", ip, names);
+    let names = all_names.join(" ");
+    println!("Adding {} {} to /etc/hosts", ip, names);
 
-  let computed_comment = match comment {
-    "" => &names,
-    _ => comment,
-  };
-  let host_line = format!("{} {} #{}", ip, names, computed_comment);
-  hosts_file.add_line(&host_line);
-  hosts_file.save();
+    let computed_comment = match comment {
+        "" => &names,
+        _ => comment,
+    };
+    let host_line = format!("{} {} #{}", ip, names, computed_comment);
+    hosts_file.add_line(&host_line);
+    hosts_file.save();
 }
 
 pub fn remove(host: &str) {
-  let mut hosts_file = crate::hostsfile::ManagedHostsFile::must_load();
-  if !hosts_file.has_host(host) {
-    println!("{} not in hosts file.", host);
-    return;
-  }
-  println!("Removing host {}", host);
-  hosts_file.remove_host(host);
-  hosts_file.save();
+    let mut hosts_file = crate::hostsfile::ManagedHostsFile::must_load();
+    if !hosts_file.has_host(host) {
+        println!("{} not in hosts file.", host);
+        return;
+    }
+    println!("Removing host {}", host);
+    hosts_file.remove_host(host);
+    hosts_file.save();
 }
 
 pub fn disable(host: &str) {
-  let mut hosts_file = crate::hostsfile::ManagedHostsFile::must_load();
-  if !hosts_file.has_host(host) {
-    if hosts_file.has_disabled_host(host) {
-      println!("{} is already disabled in hosts file.", host);
-    } else {
-      println!("{} is not in hosts file.", host);
+    let mut hosts_file = crate::hostsfile::ManagedHostsFile::must_load();
+    if !hosts_file.has_host(host) {
+        if hosts_file.has_disabled_host(host) {
+            println!("{} is already disabled in hosts file.", host);
+        } else {
+            println!("{} is not in hosts file.", host);
+        }
+        return;
     }
-    return;
-  }
-  println!("Disabling host {}", host);
-  hosts_file.disable_host(host);
-  hosts_file.save();
+    println!("Disabling host {}", host);
+    hosts_file.disable_host(host);
+    hosts_file.save();
 }
 
 pub fn enable(host: &str) {
-  let mut hosts_file = crate::hostsfile::ManagedHostsFile::must_load();
-  if !hosts_file.has_disabled_host(host) {
-    if hosts_file.has_host(host) {
-      println!("{} is already enabled in hosts file.", host);
-    } else {
-      println!("{} is not in hosts file.", host);
+    let mut hosts_file = crate::hostsfile::ManagedHostsFile::must_load();
+    if !hosts_file.has_disabled_host(host) {
+        if hosts_file.has_host(host) {
+            println!("{} is already enabled in hosts file.", host);
+        } else {
+            println!("{} is not in hosts file.", host);
+        }
+        return;
     }
-    return;
-  }
-  println!("Enabling host {}", host);
-  hosts_file.enable_host(host);
-  hosts_file.save();
+    println!("Enabling host {}", host);
+    hosts_file.enable_host(host);
+    hosts_file.save();
 }
 
 pub fn update() {
-  let target = self_update::get_target().expect("Error getting self-update target");
-  let status = self_update::backends::github::Update::configure()
-    .expect("error configuring backend")
-    .repo_owner("lucascaro")
-    .repo_name("hostman")
-    .target(&target)
-    .bin_name("hostman")
-    .show_download_progress(true)
-    .current_version(cargo_crate_version!())
-    .build()
-    .expect("cannot build")
-    .update()
-    .expect("cannot update");
-  println!("Update status: `{}`!", status.version());
+    let target = self_update::get_target().expect("Error getting self-update target");
+    let status = self_update::backends::github::Update::configure()
+        .expect("error configuring backend")
+        .repo_owner("lucascaro")
+        .repo_name("hostman")
+        .target(&target)
+        .bin_name("hostman")
+        .show_download_progress(true)
+        .current_version(cargo_crate_version!())
+        .build()
+        .expect("cannot build")
+        .update()
+        .expect("cannot update");
+    println!("Update status: `{}`!", status.version());
 }
--- src/hostsfile.rs
+++ src/hostsfile.rs
@@ -6,149 +6,146 @@ const HOSTS_FILE: &str = "/etc/hosts";
 // const HOSTS_FILE: &str = "./hosts";
 
 pub enum MatchType {
-  Partial,
-  Exact,
+    Partial,
+    Exact,
 }
 
 impl MatchType {
-  pub fn from_bool(exact: bool) -> MatchType {
-    return match exact {
-      true => MatchType::Exact,
-      false => MatchType::Partial,
-    };
-  }
+    pub fn from_bool(exact: bool) -> MatchType {
+        if exact {
+            MatchType::Exact
+        } else {
+            MatchType::Partial
+        }
+    }
 }
 
 pub struct ManagedHostsFile<'a> {
-  lines: Vec<parse_hosts::Line<'a>>,
+    lines: Vec<parse_hosts::Line<'a>>,
 }
 
 impl<'a> ManagedHostsFile<'a> {
-  pub fn load() -> Result<ManagedHostsFile<'a>, std::io::Error> {
-    let contents = read_hosts();
-    let hf = HostsFile::read_buffered(contents.as_bytes());
-    return Ok(ManagedHostsFile {
-      lines: hf.lines().map(|l| l.unwrap()).collect(),
-    });
-  }
-
-  pub fn must_load() -> ManagedHostsFile<'a> {
-    return ManagedHostsFile::load().unwrap();
-  }
-
-  pub fn get_matches(&self, host: &str, exact: MatchType) -> Vec<String> {
-    let lines: Vec<String> = self.lines.iter().map(|l| format!("{}", l)).collect();
-    lines
-      .into_iter()
-      .filter(|line| match exact {
-        MatchType::Exact => exact_match(host, line),
-        MatchType::Partial => line.contains(host),
-      })
-      .collect()
-  }
-
-  pub fn has_host(&self, host: &str) -> bool {
-    self.lines.iter().any(|l| l.hosts().any(|h| h == host))
-  }
-
-  pub fn has_disabled_host(&self, host: &str) -> bool {
-    self.lines.iter().any(|l| match l.comment() {
-      Some(c) => c.contains(host),
-      _ => false,
-    })
-  }
-
-  pub fn add_line(&mut self, line: &'a str) {
-    let l = parse_hosts::Line::new(line).unwrap();
-    self.lines.push(l);
-  }
-
-  pub fn remove_host(&mut self, host: &str) {
-    let index = self
-      .lines
-      .iter()
-      .position(|l| l.hosts().any(|h| h == host))
-      .unwrap();
-    self.lines.remove(index);
-  }
-
-  pub fn disable_host(&mut self, host: &str) {
-    let position = self
-      .lines
-      .iter()
-      .position(|l: &parse_hosts::Line| l.hosts().any(|h| h == host));
-
-    if let Some(index) = position {
-      let comment = format!("{}", self.lines[index]);
-      let new_line = parse_hosts::Line::from_comment(&comment).into_owned();
-      self.lines[index] = new_line;
-    } else {
-      println!("Error, line not found for {}", host)
+    pub fn load() -> Result<ManagedHostsFile<'a>, std::io::Error> {
+        let contents = read_hosts();
+        let hf = HostsFile::read_buffered(contents.as_bytes());
+        Ok(ManagedHostsFile {
+            lines: hf.lines().map(|l| l.unwrap()).collect(),
+        })
     }
-  }
 
-  pub fn enable_host(&mut self, host: &str) {
-    let position = self.lines.iter().position(|l| match l.comment() {
-      Some(c) => c.contains(host),
-      _ => false,
-    });
-
-    if let Some(index) = position {
-      let comment = self.lines[index].comment().unwrap();
-      let new_line = parse_hosts::Line::new(&comment).unwrap().into_owned();
-      self.lines[index] = new_line;
-    } else {
-      println!("Error, line not found for {}", host)
+    pub fn must_load() -> ManagedHostsFile<'a> {
+        ManagedHostsFile::load().unwrap()
     }
-  }
 
-  pub fn without_comments(&self) -> Vec<String> {
-    self
-      .lines
-      .iter()
-      .filter(|l| match l.data() {
-        Some(_) => true,
-        None => false,
-      })
-      .map(|l| format!("{}", l))
-      .collect::<Vec<String>>()
-  }
+    pub fn get_matches(&self, host: &str, exact: MatchType) -> Vec<String> {
+        let lines: Vec<String> = self.lines.iter().map(|l| format!("{}", l)).collect();
+        lines
+            .into_iter()
+            .filter(|line| match exact {
+                MatchType::Exact => exact_match(host, line),
+                MatchType::Partial => line.contains(host),
+            })
+            .collect()
+    }
 
-  pub fn save(&self) {
-    let file_content = format!("{}", self);
-    write_hosts(&file_content);
-  }
+    pub fn has_host(&self, host: &str) -> bool {
+        self.lines.iter().any(|l| l.hosts().any(|h| h == host))
+    }
+
+    pub fn has_disabled_host(&self, host: &str) -> bool {
+        self.lines.iter().any(|l| match l.comment() {
+            Some(c) => c.contains(host),
+            _ => false,
+        })
+    }
+
+    pub fn add_line(&mut self, line: &'a str) {
+        let l = parse_hosts::Line::new(line).unwrap();
+        self.lines.push(l);
+    }
+
+    pub fn remove_host(&mut self, host: &str) {
+        let index = self
+            .lines
+            .iter()
+            .position(|l| l.hosts().any(|h| h == host))
+            .unwrap();
+        self.lines.remove(index);
+    }
+
+    pub fn disable_host(&mut self, host: &str) {
+        let position = self
+            .lines
+            .iter()
+            .position(|l: &parse_hosts::Line| l.hosts().any(|h| h == host));
+
+        if let Some(index) = position {
+            let comment = format!("{}", self.lines[index]);
+            let new_line = parse_hosts::Line::from_comment(&comment).into_owned();
+            self.lines[index] = new_line;
+        } else {
+            println!("Error, line not found for {}", host)
+        }
+    }
+
+    pub fn enable_host(&mut self, host: &str) {
+        let position = self.lines.iter().position(|l| match l.comment() {
+            Some(c) => c.contains(host),
+            _ => false,
+        });
+
+        if let Some(index) = position {
+            let comment = self.lines[index].comment().unwrap();
+            let new_line = parse_hosts::Line::new(&comment).unwrap().into_owned();
+            self.lines[index] = new_line;
+        } else {
+            println!("Error, line not found for {}", host)
+        }
+    }
+
+    pub fn without_comments(&self) -> Vec<String> {
+        self.lines
+            .iter()
+            .filter(|l| l.data().is_some())
+            .map(|l| format!("{}", l))
+            .collect::<Vec<String>>()
+    }
+
+    pub fn save(&self) {
+        let file_content = format!("{}", self);
+        write_hosts(&file_content);
+    }
 }
 
 fn exact_match(needle: &str, haystack: &str) -> bool {
-  let exact_matcher = Regex::new(format!(r"(^| ){}( |$)", needle).as_str()).unwrap();
-  return exact_matcher.is_match(haystack);
+    let exact_matcher = Regex::new(format!(r"(^| ){}( |$)", needle).as_str()).unwrap();
+    exact_matcher.is_match(haystack)
 }
 
 fn read_hosts() -> String {
-  return std::fs::read_to_string(HOSTS_FILE).expect("could not read file");
+    std::fs::read_to_string(HOSTS_FILE).expect("could not read file")
 }
 
 fn backup_hosts() {
-  let file_name = format!("{}.bak", HOSTS_FILE);
-  if std::fs::copy(HOSTS_FILE, &file_name).is_err() {
-    eprintln!("Error: cannot write to backup file: {}", file_name);
-    std::process::exit(1);
-  }
+    let file_name = format!("{}.bak", HOSTS_FILE);
+    if std::fs::copy(HOSTS_FILE, &file_name).is_err() {
+        eprintln!("Error: cannot write to backup file: {}", file_name);
+        std::process::exit(1);
+    }
 }
 
 fn write_hosts(contents: &str) {
-  backup_hosts();
-  if std::fs::write(HOSTS_FILE, contents).is_err() {
-    eprintln!("Error: cannot write hosts file: {}", HOSTS_FILE);
-    std::process::exit(1);
-  }
+    backup_hosts();
+    if std::fs::write(HOSTS_FILE, contents).is_err() {
+        eprintln!("Error: cannot write hosts file: {}", HOSTS_FILE);
+        std::process::exit(1);
+    }
 }
 
 impl<'a> fmt::Display for ManagedHostsFile<'a> {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    let hf = &self.lines;
-    let lines: Vec<String> = hf.iter().map(|l| format!("{}", l)).collect();
-    write!(f, "{}\n", lines.join("\n"))
-  }
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let hf = &self.lines;
+        let lines: Vec<String> = hf.iter().map(|l| format!("{}", l)).collect();
+        write!(f, "{}\n", lines.join("\n"))
+    }
 }
